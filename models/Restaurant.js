08b1a007be6262c04567c70da97fa590892685da
arpitjain1405 <arpitjain1405@gmail.com>
2025-06-10 17:19:19 +0530

    Created Restaurant model

---
+++ models/Restaurant.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+exports.validateResDetails = function (resDetails) {
+  const schema = Joi.object({
+    name: Joi.string().required().min(5).max(50),
+    address: Joi.string().min(10).required(),
+    cuisine: Joi.array().items(Joi.string().required().min(3).max(30)).required(),
+    isOpen: Joi.boolean()
+  });
+};
+
+const restaurantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 5,
+    max: 50,
+  },
+  address: {
+    type: String,
+    required: true,
+    min: 10,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  cuisine: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+  ],
+  rating: Number,
+  isOpen: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+exports.Restaurant = mongoose.model("Restaurant", restaurantSchema);
